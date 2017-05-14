d29359995d4d7e78888b6680f12b6e3fb193fe9a
Maxim <mkulabin@icrealtech.com>
2017-05-14 23:15:06 +0500

    init

---
+++ scripts/AudioBufferCreator.js
@@ -0,0 +1,54 @@
+var AudioBufferCreator = function (context) {
+    var analyser = context.createAnalyser();
+    analyser.connect(context.destination);
+// load the sound
+// create a buffer source node
+    var audioBuffer;
+    var sourceNode = context.createBufferSource();
+// and connect to destination
+    sourceNode.connect(context.destination);
+    sourceNode.connect(analyser);
+    loadSound("song.mp3");
+// load the specified sound
+    function loadSound(url) {
+        var request = new XMLHttpRequest();
+        request.open('GET', url, true);
+        request.responseType = 'arraybuffer';
+        // When loaded decode the data
+        request.onload = function() {
+            // decode the data
+            context.decodeAudioData(request.response, function(buffer) {
+                // when the audio is decoded play the sound
+                playSound(buffer);
+            }, onError);
+        };
+        request.send();
+    }
+    function playSound(buffer) {
+        sourceNode.buffer = buffer;
+        sourceNode.start(0);
+    }
+// log if an error occurs
+    function onError(e) {
+        console.log(e);
+    }
+
+    // var freqs = new Uint8Array(this.analyser.frequencyBinCount);
+    // var times = new Uint8Array(this.analyser.frequencyBinCount);
+
+    fFrequencyData = new Float32Array(analyser.frequencyBinCount);
+    bFrequencyData = new Uint8Array(analyser.frequencyBinCount);
+    bTimeData = new Uint8Array(analyser.frequencyBinCount);
+    // Получаем данные
+    analyser.getFloatFrequencyData(fFrequencyData);
+    analyser.getByteFrequencyData(bFrequencyData);
+    analyser.getByteTimeDomainData(bTimeData);
+    var bufferLength = analyser.frequencyBinCount;
+    dataArray = new Uint8Array(analyser.frequencyBinCount);
+    analyser.getByteFrequencyData(dataArray);
+    return dataArray
+};
+
+// var getBuffer = function () {
+//     return dataArray;
+// };
