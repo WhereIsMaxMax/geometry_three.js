4323a38fc35cf3c5477d7bc1ee21ddcaa1b48f05
artur_gazetdinov <artur@gazetdinov.ru>
2017-05-27 18:45:03 +0500

    onpress space

--- assets/js/App.js
+++ assets/js/App.js
@@ -1,1 +1,10 @@
-gameController = new GameController(new AudioContext());
+var gameController = new GameController(new AudioContext());
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.addEventListener('keydown', function (e) {
+    if (e.code === 'Space'){
+      e.preventDefault();
+      onSpacePress(gameController);
+    }
+  });
+});
